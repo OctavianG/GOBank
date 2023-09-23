60440ba9e884c30b5179c2fdd1cc62d42b8ffb3b
OctavianG <octavian7.g@gmail.com>
2023-09-23 22:31:21 +0300

    created views and stages

--- src/main/java/bank/gobank/Controllers/Client/DashboardController.java
+++ src/main/java/bank/gobank/Controllers/Client/DashboardController.java
@@ -24,6 +24,6 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+
     }
 }
--- src/main/java/bank/gobank/Models/Model.java
+++ src/main/java/bank/gobank/Models/Model.java
@@ -1,4 +1,25 @@
 package bank.gobank.Models;
 
+import bank.gobank.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+    // singleton
+    private Model() {
+        this.viewFactory = new ViewFactory();
+    }
+
+    public static synchronized Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
 }
