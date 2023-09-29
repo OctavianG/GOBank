22f606dc169d28f139ddd443b1ffb1b82b63e48e
OctavianG <octavian7.g@gmail.com>
2023-09-29 19:44:35 +0300

    added TransactionCell

---
+++ src/main/java/bank/gobank/Controllers/Client/TransactionCellController.java
@@ -0,0 +1,13 @@
+package bank.gobank.Controllers.Client;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransactionCellController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
